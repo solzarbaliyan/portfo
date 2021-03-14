1c6e2ea787fa4a928d989ba8882152eb034519fb
solzar baliyan <ch.solzarbaliyan007@gmail.com>
2021-03-14 12:22:42 +0530

    my portfolio website

---
+++ server.py
@@ -0,0 +1,85 @@
+#Solzar@47
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+ 
+
+
+@app.route('/blog')
+def blog():
+    return render_template('index.html')
+
+
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/works.html')
+def work():
+    return render_template('works.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/blog/2021/cars')
+def blog2():
+    return 'These are my Cars BaBy......'
+
+
+def write_to_file(data):
+	with open('database.txt', mode='a') as database:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+	with open('database.csv', mode='a') as database2:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email, subject, message])
+	
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	
+    #error = None
+    if request.method == 'POST':
+    	try:
+    		data =request.form.to_dict()
+    		write_to_csv(data)
+    		return redirect('/thankyou.html')
+    	except:
+    		return'Did not save to Database'
+    else:
+    	return 'Something went wrong. Try Again!'
+
+        #if valid_login(request.form['username'],
+                       #request.form['password']):
+       #     return log_the_user_in(request.form['username'])
+      #  else:
+     #       error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+   			
+     #render_template('login.html', error=error)
